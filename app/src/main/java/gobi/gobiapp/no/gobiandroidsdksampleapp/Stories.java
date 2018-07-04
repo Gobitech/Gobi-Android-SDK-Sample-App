fd1fadd02db169938e09901b93f6945c181b87a5
Kristian Rekstad <krirek@msn.com>
2018-07-04 17:16:39 +0200

    Store story key

---
+++ app/src/main/java/gobi/gobiapp/no/gobiandroidsdksampleapp/Stories.java
@@ -0,0 +1,12 @@
+package gobi.gobiapp.no.gobiandroidsdksampleapp;
+
+/**
+ * Contains all stories we can show.
+ *
+ * @author Kristian 'krissrex' Rekstad
+ */
+public class Stories {
+
+    /** "Team Gobi" story */
+    public static final String TEAM_GOBI = "OWEXMJUWM2FKMTE5M2U3NWIXZTIZZDK0NJQ2NJUYNZRKOGZHNZM1ZJFINWVHMJBK";
+}
