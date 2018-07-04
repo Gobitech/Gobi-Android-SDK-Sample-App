329928f5a5ee0ae9a2f9652dd7e993d43cdcc934
Kristian Rekstad <krirek@msn.com>
2018-07-04 17:14:38 +0200

    Create Application subclass

---
+++ app/src/main/java/gobi/gobiapp/no/gobiandroidsdksampleapp/SdkSampleApplication.java
@@ -0,0 +1,13 @@
+package gobi.gobiapp.no.gobiandroidsdksampleapp;
+
+import android.app.Application;
+
+/**
+ * The Application instance is a singleton registered in {@code AndroidManifest.xml}.
+ * Initialize Gobi in {@link #onCreate()}.
+ *
+ * @author Kristian 'krissrex' Rekstad
+ */
+public class SdkSampleApplication extends Application {
+
+}
