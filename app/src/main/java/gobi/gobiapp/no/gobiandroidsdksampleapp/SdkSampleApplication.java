92317156e8affdfd365b9005f460d93da57306fc
Kristian Rekstad <krirek@msn.com>
2018-07-04 17:15:44 +0200

    Initialize Gobi

--- app/src/main/java/gobi/gobiapp/no/gobiandroidsdksampleapp/SdkSampleApplication.java
+++ app/src/main/java/gobi/gobiapp/no/gobiandroidsdksampleapp/SdkSampleApplication.java
@@ -2,6 +2,8 @@ package gobi.gobiapp.no.gobiandroidsdksampleapp;
 
 import android.app.Application;
 
+import no.gobiapp.gobi.sdk.Gobi;
+
 /**
  * The Application instance is a singleton registered in {@code AndroidManifest.xml}.
  * Initialize Gobi in {@link #onCreate()}.
@@ -15,5 +17,6 @@ public class SdkSampleApplication extends Application {
         super.onCreate();
 
         final String gobiCustomerId = "ZDY5OGI0YZQ3MJG0MWE3ZDA0YWEXZDLK"; // "Sample SDK app" customer
+        Gobi.with(this, gobiCustomerId);
     }
 }
